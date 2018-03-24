fd2de17e39b62c67b4a1c3861775ab2482ce889b
cash <cash@cashwu.com>
2018-03-24 10:16:40 +0800

    find products

--- LinqTests/UnitTest1.cs
+++ LinqTests/UnitTest1.cs
@@ -1,7 +1,10 @@
-﻿using ExpectedObjects;
+﻿using System;
+using ExpectedObjects;
 using LinqTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Linq;
+using NSubstitute;
 
 namespace LinqTests
 {
@@ -16,19 +19,49 @@ namespace LinqTests
 
             var expected = new List<Product>()
             {
-                //todo
+                new Product {Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo"},
+                new Product {Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e"},
+                new Product {Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e"}
             };
 
-            expected.ToExpectedObject().ShouldEqual(actual);
+            foreach (var item in actual)
+            {
+                Console.WriteLine(item.Price);
+            }
+
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
+        public void find_products_using_where_that_price_between_200_and_500()
+        {
+            var products = RepositoryFactory.GetProducts();
+            var actual = products.Where(a => a.Price > 200 && a.Price < 500);
+
+            var expected = new List<Product>()
+            {
+                new Product {Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo"},
+                new Product {Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e"},
+                new Product {Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e"}
+            };
+
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
     }
 }
 
 internal class WithoutLinq
 {
-    public static List<Product> FindProductByPrice(IEnumerable<Product> products, int lowBoundary, int highBoundary)
+    public static IEnumerable<Product> FindProductByPrice(IEnumerable<Product> products, int lowBoundary,
+        int highBoundary)
     {
-        throw new System.NotImplementedException();
+        foreach (var product in products)
+        {
+            if (product.Price > lowBoundary && product.Price < highBoundary)
+            {
+                yield return product;
+            }
+        }
     }
 }
 
