6e7d8bff9e554efdd25239f3ccd16474dd7e23c2
cash <cash@cashwu.com>
2018-03-24 12:28:41 +0800

    your own linq where

--- LinqTests/UnitTest1.cs
+++ LinqTests/UnitTest1.cs
@@ -56,6 +56,32 @@ namespace LinqTests
         }
 
         [TestMethod]
+        public void find_employee_that_age_more_30_item_index_more_2()
+        {
+            var employees = RepositoryFactory.GetEmployees();
+            var actual = employees.FindProductByPrice((e, idx) => e.Age > 30 && idx >= 2);
+
+            var expected = new List<Employee>()
+            {
+                //new Employee {Name = "Joe", Role = RoleType.Engineer, MonthSalary = 100, Age = 44, WorkingYear = 2.6},
+                //new Employee {Name = "Tom", Role = RoleType.Engineer, MonthSalary = 140, Age = 33, WorkingYear = 2.6},
+                new Employee {Name = "Kevin", Role = RoleType.Manager, MonthSalary = 380, Age = 55, WorkingYear = 2.6},
+                //new Employee {Name = "Andy", Role = RoleType.OP, MonthSalary = 80, Age = 22, WorkingYear = 2.6},
+                new Employee {Name = "Bas", Role = RoleType.Engineer, MonthSalary = 280, Age = 36, WorkingYear = 2.6},
+                //new Employee {Name = "Mary", Role = RoleType.OP, MonthSalary = 180, Age = 26, WorkingYear = 2.6},
+                //new Employee {Name = "Frank", Role = RoleType.Engineer, MonthSalary = 120, Age = 16, WorkingYear = 2.6},
+                new Employee {Name = "Joey", Role = RoleType.Engineer, MonthSalary = 250, Age = 40, WorkingYear = 2.6},
+            };
+
+            //foreach (var item in actual)
+            //{
+            //    Console.WriteLine(item.Price);
+            //}
+
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [TestMethod]
         public void find_products_using_where_that_price_between_200_and_500()
         {
             var products = RepositoryFactory.GetProducts();
@@ -75,8 +101,7 @@ namespace LinqTests
 
 internal static class WithoutLinq
 {
-    public static IEnumerable<T> FindProductByPrice<T>(this IEnumerable<T> products,
-        Func<T, bool> predicate)
+    public static IEnumerable<T> FindProductByPrice<T>(this IEnumerable<T> products, Func<T, bool> predicate)
     {
         foreach (var product in products)
         {
@@ -86,8 +111,48 @@ internal static class WithoutLinq
             }
         }
     }
+
+    public static IEnumerable<T> FindProductByPrice<T>(this IEnumerable<T> products, Func<T, int, bool> predicate)
+    {
+        var index = 0;
+
+        foreach (var product in products)
+        {
+            if (predicate(product, index))
+            {
+                yield return product;
+            }
+
+            index++;
+        }
+    }
 }
 
 internal static class YourOwnLinq
 {
+    public static IEnumerable<T> CashWhere<T>(this IEnumerable<T> elements, Func<T, bool> predicate)
+    {
+        foreach (var element in elements)
+        {
+            if (predicate(element))
+            {
+                yield return element;
+            }
+        }
+    }
+
+    public static IEnumerable<T> CashWhere<T>(this IEnumerable<T> elements, Func<T, int, bool> predicate)
+    {
+        var index = 0;
+
+        foreach (var element in elements)
+        {
+            if (predicate(element, index))
+            {
+                yield return element;
+            }
+
+            index++;
+        }
+    }
 }
